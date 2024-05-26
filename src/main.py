e6fc2629c60328c01b7d5c1f53531736da1ddfe4
ArcRoot1379 <phg5253@gmail.com>
2024-05-26 19:24:51 +0900

    fix: read state logic

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
-###def input():
-#        with open('C:\\Users\\phg52\\OneDrive\\바탕 화면\\compiler_term_proj\\24-1-CAU-CSE-Compiler-Term-Project-SLR-parser\\src\\input.txt', 'r', encoding='utf-8') as file:
-#            # 파일 내용 전체를 읽어서 변수에 저장
-#            content = file.read()
-#        tokens = content.split()
-#        print(tokens)    
-#        return tokens
+import sys
+from data import rules, parse_table
+
 class SLRParser:
     def __init__(self, parse_table, rules):
         self.parse_table = parse_table
@@ -28,17 +24,18 @@ class SLRParser:
 
             if action.startswith('s'):
                 self.inputStack.append(current_token)
-                self.statestack.append(int(action[1]))
+                self.statestack.append(int(action[1:]))
                 token_index+=1
             elif action.startswith('r'):
-                for _ in range(self.rules[int(action[1])][1]):
+                for _ in range(self.rules[int(action[1:])][1]):
                     self.statestack.pop()
-                    print(self.rules[int(action[1])][1])
+                    print(self.rules[int(action[1:])][1])
                     print(f'pop:{self.inputStack.pop()}')
                 self.inputStack.append(self.rules[int(action[1])][0])
                 current_state=self.statestack[-1]
                 current_token=self.inputStack[-1]
                 action=self.parse_table[current_state][current_token]
+                #goto case임
                 self.statestack.append(int(action))
             elif action=='acc':
                 print('Accept')
@@ -47,31 +44,26 @@ class SLRParser:
             print(f'input stack: {self.inputStack}')
             print()
 
-# 문법 규칙
-rules = [
-    ('E', 1),  # E -> T
-    ('T', 3),  # T -> T * F
-    ('T', 1),  # T -> F
-    ('F', 3),  # F -> ( T )
-    ('F', 1)   # F -> id
-]
+def main():
+    parser = SLRParser(parse_table, rules)
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <input_file>")
+        return
+    
+    input_file = sys.argv[1]
+    
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+    
+    for line in lines:
+        tokens = line.strip().split()
+        if parser.parse(tokens):
+            print("ACCEPT")
+        else:
+            print("REJECT")
 
-# 파서 테이블 (예제에서는 고정된 테이블 사용)
-parse_table = [
-    { '(': 's3', 'id': 's4', 'T': 1, 'F': 2},
-    {'*': 's5', '$': 'acc'},
-    {'*': 'r2', ')': 'r2', '$': 'r2'},  # Changed r2 to r4
-    {'(': 's3','id': 's4', 'T': 6, 'F': 2},  # Changed T to 7
-    {'*': 'r4', ')': 'r4', '$':'r4'},
-    {'(': 's3', 'id': 's4', 'F':7},  # Changed r6 to r5 and removed '+'
-    {'*': 's5', ')': 's8'},
-    {'*': 'r1', ')': 'r1', '$': 'r1'},
-    {'*': 'r3', ')': 'r3', '$': 'r3'},
-]
+if __name__ == "__main__":
+    main()
 
-# 토큰 예제 (id + id * id)
-tokens = ['id', '*', 'id', '$']
 
-parser = SLRParser(parse_table, rules)
-parser.parse(tokens)
 
